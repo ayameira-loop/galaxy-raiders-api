ad71ae8cebf69e3137e188279c08e4c5c0da6fde
ayaes <72822950+ayameira-loop@users.noreply.github.com>
2023-06-18 21:22:25 -0300

    Update Explosion class to include a timer and set the duration of each explosion

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -2,15 +2,29 @@ package galaxyraiders.core.game
 
 import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
+import java.util.Timer
+import java.util.TimerTask
+
 
 class Explosion(
   initialPosition: Point2D
 ) :
-  SpaceObject("Explosion", '*', initialPosition, Vector2D(0.0,0.0), 1.0, 0.0){//ver qual raio é o melhor
-    var is_trigged = false
+  SpaceObject("Explosion", '*', initialPosition, Vector2D(0.0,0.0), 1.0, 0.0) {//ver qual raio é o melhor
 
-    fun was_exploded(){
-        is_trigged = true
+    var isTriggered: Boolean = true
+      private set
+
+    fun untrigger() {
+      this.isTriggered = false
     }
-    
+
+    init {
+      var timerTask = object : TimerTask() {
+        override fun run() {
+          untrigger()
+        }
+      }
+
+      Timer().schedule(timerTask, 2000)
+    }
   }
